5ea78f8757c1a6b32f6ad47df75f58336f4ac052
tshepithekiso <tshepangthekiso15@gmail.com>
2024-07-22 18:55:40 +0000

    task_3

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,46 +1,25 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: {
-        header: './modules/header/header.js',
-        body: './modules/body/body.js',
-        footer: './modules/footer/footer.js',
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'public'),
-    },
-    mode: 'development',
-    devServer: {
-        static: './public',
-        port: 8564,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[path][name].[ext]',
-                            outputPath: 'assets/',
-                            publicPath: 'assets/',
-                        },
-                    },
-                ],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin(),
-        new CleanWebpackPlugin(),
+  mode: 'production', // Set the mode to production
+  entry: {
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'), // Output directory
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Regex to match CSS files
+        use: ['style-loader', 'css-loader'], // Loaders to use
+      },
+      // Other rules...
     ],
-    devtool: 'inline-source-map',
+  },
+  // Other configurations...
 };
+
