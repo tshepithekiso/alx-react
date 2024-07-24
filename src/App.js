728e0fd1e1ccc095a2fb16db9dceca65e79849fa
tshepithekiso <tshepangthekiso15@gmail.com>
2024-07-24 08:41:24 -0700

    dashboard

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import logo from './holberton_logo.png'; // Adjust the path if necessary
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <img src={logo} alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <footer className="App-footer">
+        <p>Copyright 2020 - Holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
