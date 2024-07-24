a5a8959c815def3e068e0420c4e33bf196a944ec
tshepithekiso <tshepangthekiso15@gmail.com>
2024-07-24 16:04:18 -0700

    task_3

---
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import logo from './holberton_logo.png';
+import { getFullYear, getFooterCopy } from './utils'; // Import utility functions
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <img src={logo} alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+
+        {/* Email input */}
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+
+        {/* Password input */}
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+
+        {/* OK button */}
+        <button type="button">OK</button>
+      </div>
+      <footer className="App-footer">
+        <p>{getFooterCopy(true)}</p>
+        <p>Copyright {getFullYear()} - {getFooterCopy(false)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
