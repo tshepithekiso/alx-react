4e986514e74dde8288fe17ebf84ffbac87e91522
tshepithekiso <tshepangthekiso15@gmail.com>
2024-07-25 18:54:35 +0000

    task_5

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
+
---
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -0,0 +1,5 @@
+import './styles.css'; // Import CSS file if you have one
+
+const app = document.getElementById('app');
+app.innerHTML = `<h1>Hello, Webpack!</h1>`;
+
