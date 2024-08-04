7003d6c67e35c6f0ab6faf32524939f91e239607
tshepithekiso <tshepangthekiso15@gmail.com>
2024-08-04 21:27:53 +0000

    task_3/dashboard

--- 0x03-React_component/task_3/dashboard/src/App/App.js
+++ 0x03-React_component/task_3/dashboard/src/App/App.js
@@ -1,79 +1,49 @@
 // App.js
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import CourseList from '../CourseList/CourseList';
-import Login from '../Login/Login';
 import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import CourseList from '../CourseList/CourseList';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import BodySection from '../BodySection/BodySection';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-  }
-
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown(event) {
-    if (event.ctrlKey && event.key === 'h') {
-      alert('Logging you out');
-      this.props.logOut();
-    }
+    this.state = {
+      isLoggedIn: false,
+    };
   }
 
   render() {
-    const { isLoggedIn, listCourses, listNotifications } = this.props;
+    const { isLoggedIn } = this.state;
+
     return (
-      <>
-        <Notifications displayDrawer={true} listNotifications={listNotifications} />
-        <div className="App">
-          <Header />
-          {isLoggedIn ? (
-            <CourseList listCourses={listCourses} />
-          ) : (
+      <div className="App">
+        <Notifications />
+        <Header />
+        {isLoggedIn ? (
+          <BodySectionWithMarginBottom title="Course list">
+            <CourseList />
+          </BodySectionWithMarginBottom>
+        ) : (
+          <BodySectionWithMarginBottom title="Log in to continue">
             <Login />
-          )}
-          <Footer />
-        </div>
-      </>
+          </BodySectionWithMarginBottom>
+        )}
+        <BodySection title="News from the School">
+          <p>
+            Welcome to our school! Stay updated with the latest news and
+            announcements. Explore upcoming events, achievements, and more.
+          </p>
+        </BodySection>
+        <Footer />
+      </div>
     );
   }
 }
-
-App.propTypes = {
-  isLoggedIn: PropTypes.bool,
-  listCourses: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-      credit: PropTypes.number.isRequired,
-    })
-  ),
-  listNotifications: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      html: PropTypes.shape({
-        __html: PropTypes.string.isRequired,
-      }).isRequired,
-      type: PropTypes.string.isRequired,
-      value: PropTypes.string,
-    })
-  ),
-  logOut: PropTypes.func,
-};
-
-App.defaultProps = {
-  isLoggedIn: false,
-  listCourses: [],
-  listNotifications: [],
-  logOut: () => {},
-};
 
 export default App;
 
