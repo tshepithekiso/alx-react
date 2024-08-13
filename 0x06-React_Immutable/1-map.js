d84cca444ac5229a4cee14956aad45ed932b5591
tshepithekiso <tshepangthekiso15@gmail.com>
2024-08-13 20:25:14 +0000

    1-map.js

---
+++ 0x06-React_Immutable/1-map.js
@@ -0,0 +1,22 @@
+// Importing the Map from Immutable.js
+const { Map } = require('immutable');
+
+/**
+ * Converts a JavaScript object into an immutable Map.
+ * @param {Object} object - The object to convert.
+ * @returns {Map} - An immutable Map representation of the object.
+ */
+function getImmutableObject(object) {
+    return Map(object);
+}
+
+// Example usage
+const exampleObject = {
+    fear: true,
+    smell: -1033575916.9145899,
+    wall: false,
+    thing: -914767132
+};
+
+const immutableMap = getImmutableObject(exampleObject);
+console.log(immutableMap);
