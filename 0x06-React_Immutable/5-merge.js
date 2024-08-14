8ee3d524ef4285c8da92695efbbe7910cefa3160
tshepithekiso <tshepangthekiso15@gmail.com>
2024-08-14 13:53:51 +0000

    5-merge.js

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,41 +1,31 @@
-// Importing the List and Map from Immutable.js using CommonJS
-const { List, Map } = require('immutable');
+import { List, Map } from 'immutable';
 
 /**
- * Concatenates two arrays into an immutable List.
- * @param {Array} page1 - The first array.
- * @param {Array} page2 - The second array.
- * @returns {List} - An immutable List containing the values of the two pages.
+ * Function to concatenate two arrays into a List
+ * @param {Array} page1 - First array
+ * @param {Array} page2 - Second array
+ * @returns {List} - Immutable List containing values from both arrays
  */
-function concatElements(page1, page2) {
-    return List([...page1, ...page2]);
+export function concatElements(page1, page2) {
+  return List(page1).concat(List(page2));
 }
 
 /**
- * Merges two objects into an immutable List.
- * If two values are the same, values from page2 are used.
- * @param {Object} page1 - The first object.
- * @param {Object} page2 - The second object.
- * @returns {List} - An immutable List containing the values of the two pages.
+ * Function to merge two objects into a List of their values
+ * @param {Object} page1 - First object
+ * @param {Object} page2 - Second object
+ * @returns {List} - Immutable List containing merged values from both objects
  */
-function mergeElements(page1, page2) {
-    const mergedMap = Map(page1).merge(Map(page2));
-    return mergedMap.valueSeq().toList();
+export function mergeElements(page1, page2) {
+  return List(Map(page1).merge(Map(page2)).valueSeq());
 }
 
-// Export the functions using CommonJS syntax
-module.exports = {
-    concatElements,
-    mergeElements
-};
-
-// Example usage
+// Test the functions directly within the file
 const array1 = [1, 2, 3];
 const array2 = [4, 5, 6];
-const concatenatedList = concatElements(array1, array2);
-console.log(concatenatedList.toJS()); // Output: [1, 2, 3, 4, 5, 6]
+console.log(concatElements(array1, array2).toJS()); // Output: [1, 2, 3, 4, 5, 6]
 
-const object1 = { a: 1, b: 2, c: 3 };
-const object2 = { b: 4, c: 5, d: 6 };
-const mergedList = mergeElements(object1, object2);
-console.log(mergedList.toJS()); // Output: [1, 4, 5, 6]
+const object1 = { a: 1, b: 2 };
+const object2 = { b: 3, c: 4 };
+console.log(mergeElements(object1, object2).toJS()); // Output: [1, 3, 4]
+
