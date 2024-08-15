a256f62cbe39265cfaf27f381e1e97ed51db740a
tshepithekiso <tshepangthekiso15@gmail.com>
2024-08-15 13:42:39 +0000

    3-list.js

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -7,12 +7,3 @@ export function getListObject(array) {
 export function addElementToList(list, element) {
     return list.push(element);
 }
-
-const array = ['a', 'b', 'c'];
-const list = getListObject(array);
-
-console.log(list); // Should return List [ "a", "b", "c" ]
-
-const newList = addElementToList(list, 'd');
-console.log(newList); // Should return List [ "a", "b", "c", "d" ]
-
