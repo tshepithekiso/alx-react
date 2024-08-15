e335b07c3b6862ad31a4db154622eafe615594df
tshepithekiso <tshepangthekiso15@gmail.com>
2024-08-15 13:52:21 +0000

    0-fromjs.js

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,16 +1,5 @@
 import { fromJS } from 'immutable';
 
 export default function getImmutableObject(object) {
-    return fromJS(object);
+  return fromJS(object);
 }
-
-const exampleObject = {
-    fear: true,
-    smell: -1033575916.9145899,
-    wall: false,
-    thing: -914767132
-};
-
-const immutableObject = getImmutableObject(exampleObject);
-console.log(immutableObject);
-
