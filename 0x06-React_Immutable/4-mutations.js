284b6738aeda8b06b4fb2641e26a75ec097ae2b5
tshepithekiso <tshepangthekiso15@gmail.com>
2024-08-15 13:40:29 +0000

    4-mutations.js

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -14,6 +14,3 @@ export const map = Map({
 // Creating the map2 constant by modifying the map
 export const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
 
-// Example usage
-console.log(map.toJS()); // Output: { 1: 'Liam', 2: 'Noah', 3: 'Elijah', 4: 'Oliver', 5: 'Jacob', 6: 'Lucas' }
-console.log(map2.toJS()); // Output: { 1: 'Liam', 2: 'Benjamin', 3: 'Elijah', 4: 'Oliver', 5: 'Jacob', 6: 'Lucas' }
