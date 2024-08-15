2174b1a8304c23ae8a18332f5b49d2e6c4dd60d9
tshepithekiso <tshepangthekiso15@gmail.com>
2024-08-15 13:47:30 +0000

    1-map.js

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,16 +1,5 @@
 import { Map } from 'immutable';
 
-export default function getImmutableObject(object) {
-    return Map(object);
-}
+const getImmutableObject = (object) => Map(object);
 
-const exampleObject = {
-    fear: true,
-    smell: -1033575916.9145899,
-    wall: false,
-    thing: -914767132
-};
-
-const immutableMap = getImmutableObject(exampleObject);
-console.log(immutableMap);
-
+export default getImmutableObject;
