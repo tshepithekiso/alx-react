1f10180840ba21a17fa1d79d892ef041b48b8ef6
tshepithekiso <tshepangthekiso15@gmail.com>
2024-08-15 13:33:16 +0000

    7-equality.js

--- 0x06-React_Immutable/7-equality.js
+++ 0x06-React_Immutable/7-equality.js
@@ -1,37 +1,5 @@
-// Importing the Map and is from Immutable.js using CommonJS
-const { Map, is } = require('immutable');
+import { is } from 'immutable';
 
-/**
- * Checks if two Immutable Maps are equal.
- * @param {Map} map1 - The first Immutable Map.
- * @param {Map} map2 - The second Immutable Map.
- * @returns {boolean} - True if the Maps are equal, false otherwise.
- */
-function areMapsEqual(map1, map2) {
-    return is(map1, map2);
+export default function areMapsEqual(map1, map2) {
+  return is(map1, map2);
 }
-
-// Example usage
-const map1 = new Map({
-    firstName: 'Guillaume',
-    lastName: 'Salva',
-});
-
-const map2 = new Map({
-    firstName: 'Guillaume',
-    lastName: 'Salva',
-});
-
-console.log(areMapsEqual(map1, map2)); // Output: true
-
-const map3 = new Map({
-    firstName: 'John',
-    lastName: 'Doe',
-});
-
-console.log(areMapsEqual(map1, map3)); // Output: false
-
-// Export the function using CommonJS syntax
-module.exports = {
-    areMapsEqual,
-};
