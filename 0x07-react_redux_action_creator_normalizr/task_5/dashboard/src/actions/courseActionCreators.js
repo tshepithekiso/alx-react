a97f8e0b5e268fcdf0dd1f54a524e341c20eb845
tshepithekiso <tshepangthekiso15@gmail.com>
2024-08-19 19:33:45 +0000

    task_5/dashboard

---
+++ 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/actions/courseActionCreators.js
@@ -0,0 +1,14 @@
+// task_3/dashboard/src/actions/courseActionCreators.js
+
+import { SELECT_COURSE, UNSELECT_COURSE } from './courseActionTypes';
+
+export const selectCourse = (index) => ({
+  type: SELECT_COURSE,
+  index,
+});
+
+export const unSelectCourse = (index) => ({
+  type: UNSELECT_COURSE,
+  index,
+});
+
---
+++ 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/actions/notificationActionCreators.js
@@ -0,0 +1,14 @@
+// task_5/dashboard/src/actions/notificationActionCreators.js
+
+import { MARK_AS_READ, SET_TYPE_FILTER } from './notificationActionTypes';
+
+export const markAsAread = (index) => ({
+  type: MARK_AS_READ,
+  index,
+});
+
+export const setNotificationFilter = (filter) => ({
+  type: SET_TYPE_FILTER,
+  filter,
+});
+
